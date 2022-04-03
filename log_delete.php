e9155bbbd1fd32585c1655135f6ab5c5dc9e9d36
Jaeuk Ju <jujaeuk@gmail.com>
2022-04-03 14:05:54 +0900

    Update log_delete.php

--- log_delete.php
+++ log_delete.php
@@ -2,7 +2,7 @@
 include "lib.php";
 include "barrier.php";
 include "data/db_access.php";
-
+session_start();
 if(isset($_COOKIE['user'])){
 	$que="delete from ".$homename."_log where no=".$_GET['no'];
 	mysqli_query($connect,$que);
